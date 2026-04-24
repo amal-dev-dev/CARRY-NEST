d2c053b0b8ea745bb4297a44c3a8c019c914d7a6
amaldevk <amaldevk2k@gmail.com>
2026-04-24 17:27:51 +0530

    Create login page and signup page

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 import express from 'express';
-import path from 'node: path';
+import path from 'path';
 import { fileURLToPath } from 'url';
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import session from 'express-session';
 
-const app = express()
+const app = express();
 
+// Create __filename and __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);;
 
-
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'Public')));
+app.set('views', path.join(__dirname, 'Views')); 
 
-app.get('/login',(req, res) => {
-    res.send('user/login');
+app.get('/signup', (req, res) => {
+    res.render('user/signup')
 });
-
-
 app.listen(3000, ()=> {
     console.log(`Server Running on http://localhost:3000`)
 });
