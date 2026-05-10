d1f81203602c23868fa2683861dcbc94dc1cae9c
amaldevk <amaldevk2k@gmail.com>
2026-05-10 11:38:59 +0530

    completed forgot password otp flow

--- Routes/user/userRoutes.js
+++ Routes/user/userRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 const router = express.Router();
 import userLogin from '../../Controller/user/login.js';
 import userSignup from '../../Controller/user/signup.js';
+import userForgotPassword from '../../Controller/user/forgotPassword.js';
 import { requireAuth, isLoggedIn } from '../../Middleware/userAuth.js';
 
 router.get('/login', isLoggedIn, userLogin.loadLogin);
@@ -15,5 +16,13 @@ router.get('/home', requireAuth, userLogin.loadHome);
 router.get('/signup-otp', userSignup.loadVerifyOTP);
 router.post('/signup/otp', userSignup.verifyOTP);
 router.post('/resend-otp', userSignup.resendOTP);
+
+router.get('/forgot-password', userForgotPassword.loadForgotPassword);
+router.post('/forgot-password', userForgotPassword.forgotPassword);
+router.get('/forgot-otp', userForgotPassword.loadVerifyOTP);
+router.post('/forgot-otp', userForgotPassword.verifyOTP);
+router.get('/reset-password', userForgotPassword.loadResetPassword);
+router.post('/reset-password', userForgotPassword.resetPassword);
+router.post('/forgot/resend-otp', userForgotPassword.resendOTP);
 
 export default router;
